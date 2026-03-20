38fe6f84eb857f72f814dfbae79b8c41f90c56ac
mishgun <hexorand@mail.ru>
2026-03-20 23:10:52 +0300

    fix: security filter

--- src/main/java/com/poly/taskapi/auth/config/SecurityConfig.java
+++ src/main/java/com/poly/taskapi/auth/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(
                 "/api/v1/auth/**",
-                "/api/v1/api-docs/**",
+                "/v3/api-docs/**",
                 "/swagger-ui/**",
                 "/swagger-ui.html"
             ).permitAll()
